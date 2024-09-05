24c3890edd0f4edba64dcdea3812ed0582479f1b
Karina Rozenberga Korneva <karinilya@mail.ru>
2024-09-05 22:07:50 +0200

    доработка дз-2

--- src/components/SidebarItem/SidebarItem.tsx
+++ src/components/SidebarItem/SidebarItem.tsx
@@ -3,24 +3,17 @@ import styles from "./SidebarItem.module.css";
 
 export function SidebarItem() {
   return (
-    <div className={styles.mainSidebar}>
-      <div className={styles.sidebarBlock}>
-        <div className={styles.sidebarList}>
-          <div className={styles.sidebarItem}>
-            <a className={styles.sidebarLink} href='#'>
-              <Image
-                className={styles.sidebarImg}
-                src='/img/playlist01.png'
-                alt="day's playlist"
-                width={250}
-                height={170}
-                priority={true}
-              />
-            </a>
-          </div>
-          
-          </div>
-        </div>
-      </div>
+    <div className={styles.sidebarItem}>
+    <a className={styles.sidebarLink} href="#">
+      <Image
+        className={styles.sidebarImg}
+        src="/img/playlist01.png"
+        alt="day's playlist"
+        width={250}
+        height={150}
+        priority
+      />
+    </a>
+  </div>
   );
 }
